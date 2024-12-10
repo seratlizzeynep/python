41271b7fe7643e59e2db6dc55fb3fcf1cb256f78
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-12-10 21:43:09 +0300

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,37 @@
+a = 2 + 2
+
+'hello ai'
+zeynep= "*deneme*"
+zeynep.upper()
+zeynep.split("*")
+
+zeynep = "deneme1"
+zeynep [0]
+zeynep[3]
+zeynep[10]
+
+"9" + "1"
+
+ifade = "1012340"
+ifade = ifade + "1"
+ifade.strip("1")
+
+degisken = 4
+degisken*degisken
+
+ifade = "Merhaba! "
+ifade.strip("")
+
+"10" + 2 
+
+type(3.14)
+
+ifade = "Merhaba!"
+ifade = ifade.lower()
+ifade = ifade.replace("!","")
+ifade
+
+a = "bu uzun bir metindir"
+a[8]
+
+print("uzaya", "git", sep = "**")
